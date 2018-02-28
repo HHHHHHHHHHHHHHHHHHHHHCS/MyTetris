fdb56f7c7a88bf2a9a0c420eea646b697a0ac21b
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-02-28 13:37:11 +0800

    基础UI 的制作

---
+++ Assets/Scripts/Ctrl/Ctrl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ctrl : MonoBehaviour
+{
+    public Model Model { get; private set; }
+    public View View { get; private set; }
+
+	private void Awake ()
+    {
+        Model = GameObject.Find("Model").GetComponent<Model>();
+        View = GameObject.Find("View").GetComponent<View>();
+	}
+
+}
