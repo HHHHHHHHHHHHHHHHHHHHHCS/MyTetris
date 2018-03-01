c47558e781fb18ff0fd974e95d7170897f5deb3f
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-03-01 17:03:19 +0800

    添加了 FSM 状态机

--- Assets/Scripts/Ctrl/Ctrl.cs
+++ Assets/Scripts/Ctrl/Ctrl.cs
@@ -7,10 +7,28 @@ public class Ctrl : MonoBehaviour
     public Model Model { get; private set; }
     public View View { get; private set; }
 
-	private void Awake ()
+    private FSMSystem fsm;
+
+    private void Awake()
     {
         Model = GameObject.Find("Model").GetComponent<Model>();
         View = GameObject.Find("View").GetComponent<View>();
-	}
+        MakekFSM();
+    }
 
+    private void MakekFSM()
+    {
+        fsm = new FSMSystem();
+        FSMState[] states = fsm.GetComponentsInChildren<FSMState>();
+        MenuState s = null;
+        foreach (FSMState state in states)
+        {
+            fsm.AddState(state);
+            if (state is MenuState)
+            {
+                s = (MenuState)state;
+            }
+        }
+        fsm.SetCurrentState(s);
+    }
 }
